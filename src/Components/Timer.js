11550f46b0780f0725ea58751ceaa7b899d7f2c2
Esha Patel <Patellesha11@gmail.com>
2025-04-13 21:54:06 +0300

    added const

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import './Timer.css';
 
 function Timer () {
+
+    const [countdown, setCountDown] = useState(null)
     return (
         <div>
-            
+
         </div>
     )
 
