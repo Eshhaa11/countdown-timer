192cfd32fa2664b5c6c4bda5184d91a20deb8d15
Esha Patel <Patellesha11@gmail.com>
2025-04-13 22:02:38 +0300

    added seconds calculation to format function

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -18,6 +18,7 @@ function Timer ({ initialTime }) {
          const format = (seconds) => {
             const hours = Math.floor(seconds / 3600);
             const mins = Math.floor(seconds % 3600 / 60); 
+            const secs = seconds % 60;
          }
 
 
