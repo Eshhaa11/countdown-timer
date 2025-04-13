616aca5cce392a5095d0765405f4e016d83abf69
Esha Patel <Patellesha11@gmail.com>
2025-04-13 22:05:47 +0300

    style: format code for improved readability and consistency

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,37 +1,31 @@
 import { useState, useEffect, use } from "react";
-import './Timer.css';
+import "./Timer.css";
 
-function Timer ({ initialTime }) {
-    const [time, setTime] = useState(initialTime)
+function Timer({ initialTime }) {
+  const [time, setTime] = useState(initialTime);
 
-    useEffect(() => {
-        if ( time <= 0) return;
+  useEffect(() => {
+    if (time <= 0) return;
 
-        const timer = setInterval(() => {
-         setTime( t => t -1); 
-        }, 1000);
+    const timer = setInterval(() => {
+      setTime((t) => t - 1);
+    }, 1000);
 
-     
-         return () => clearInterval(timer);
-         }, [time]);
-    
-         const format = (seconds) => {
-            const hours = Math.floor(seconds / 3600);
-            const mins = Math.floor(seconds % 3600 / 60); 
-            const secs = seconds % 60;
+    return () => clearInterval(timer);
+  }, [time]);
 
-            const h = hours < 10 ? '0' + hours : hours;
-            const m = mins < 10 ? '0' + mins : mins;
-            const s = secs < 10 ? '0' + secs : secs;
-            return `${h}:${m}:${s}`;
-         };
+  const format = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const mins = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
 
+    const h = hours < 10 ? "0" + hours : hours;
+    const m = mins < 10 ? "0" + mins : mins;
+    const s = secs < 10 ? "0" + secs : secs;
+    return `${h}:${m}:${s}`;
+  };
 
-    return (
-        <div className="timer">
-            {format(time)}
-        </div>
-    );
+  return <div className="timer">{format(time)}</div>;
 }
 
 export default Timer;
